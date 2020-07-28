12fa3ae24bc9d1bd505ee1c5f6ebbda5b3ef6d05
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-07-28 15:09:54 +0000

    .

--- ConsoleAppCallApi.NET461/Program.cs
+++ ConsoleAppCallApi.NET461/Program.cs
@@ -26,14 +26,6 @@ namespace ConsoleAppCallFunctionWithAAD
             services.AddDistributedMemoryCache();
             services.AddAuditClient(o =>
             {
-                /*
-                 * "AuditClientOptions": {
-    "TenantId": "020b0cf3-d6b2-464e-9b2d-45e124244428",
-    "ClientId": "c64feb8e-4545-4f2c-a0dd-6b72a8d1a8bb",
-    "ClientSecret": "M3QN2z.KH7_M~Hm85SJ_I9Y-4IY0SIGo_~",
-    "Resource": "821eb724-edb8-4dba-b425-3f953250c0ae",
-    "BaseAddress": "https://localhost:5001"
-  }*/
                 o.BaseAddress = "https://localhost:5001";
                 o.TenantId = tenant;
                 o.ClientId = clientId;
@@ -41,19 +33,7 @@ namespace ConsoleAppCallFunctionWithAAD
                 o.Resource = "821eb724-edb8-4dba-b425-3f953250c0ae";
             });
 
-            //services.AddHostedService<Worker2>();
             services.BuildServiceProvider(unityContainer);
-
-            //var host = Host.CreateDefaultBuilder(args)
-            //    .UseSerilog()
-            //    .ConfigureServices((hostContext, services) =>
-            //    {
-            //        services.AddAuditClient(hostContext.Configuration.GetSection("AuditClientOptions"));
-
-            //        services.AddHostedService<Worker2>();
-            //    });
-
-            //host.Build().Run();
 
             var sw = unityContainer.Resolve<Worker2>();
             sw.ExecuteAsync(CancellationToken.None).Wait();
