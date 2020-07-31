8a1717118c785ad99d6855c0a4ef2776ddc2ad8e
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-07-31 11:53:45 +0000

    .

--- ConsoleAppStorageQueue/Program.cs
+++ ConsoleAppStorageQueue/Program.cs
@@ -1,28 +1,11 @@
 ﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-using Azure.Core;
 using Azure.Identity;
 using Azure.Storage;
 using Azure.Storage.Queues;
 using Azure.Storage.Sas;
-using Microsoft.Azure.Storage;
 
 namespace ConsoleAppStorageQueue
 {
-    public class T : TokenCredential
-    {
-        public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken)
-        {
-            return new AccessToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Imh1Tjk1SXZQZmVocTM0R3pCRFoxR1hHaXJuTSIsImtpZCI6Imh1Tjk1SXZQZmVocTM0R3pCRFoxR1hHaXJuTSJ9.eyJhdWQiOiIxZDYzMDljOS0zZTIyLTQyZDgtYmI1OS0zMzQxYTZmODYzMmIiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8wMjBiMGNmMy1kNmIyLTQ2NGUtOWIyZC00NWUxMjQyNDQ0MjgvIiwiaWF0IjoxNTk1ODc0ODYyLCJuYmYiOjE1OTU4NzQ4NjIsImV4cCI6MTU5NTg3ODc2MiwiYWlvIjoiRTJCZ1lMRFF1LzA1N3VQNVlsdnp5N3RtbnhRUUFBQT0iLCJhcHBpZCI6ImM2NGZlYjhlLTQ1NDUtNGYyYy1hMGRkLTZiNzJhOGQxYThiYiIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzAyMGIwY2YzLWQ2YjItNDY0ZS05YjJkLTQ1ZTEyNDI0NDQyOC8iLCJvaWQiOiJhOGE4YThlYS03YzczLTQ1MmItYTQ4ZC03ZjgyMDFlNGM0MmUiLCJzdWIiOiJhOGE4YThlYS03YzczLTQ1MmItYTQ4ZC03ZjgyMDFlNGM0MmUiLCJ0aWQiOiIwMjBiMGNmMy1kNmIyLTQ2NGUtOWIyZC00NWUxMjQyNDQ0MjgiLCJ1dGkiOiIzR2dVeTQtMWRreTFyZ3pMMDNKWkFBIiwidmVyIjoiMS4wIn0.1_BLIyW-6CdmhenzNknz6ouZGBNGx9G75ruhRcX_JwOFlyq2W3GffqKPWZ9Yb23OxeE4eSwpyh2UWwEAXbgwGq_NVASNrLw6Tn4KLl2erX6fTM11F56Vp_lqR3Fz3NJA3Hesnso19x_cinDrC_oNV8BTrtM5ltAFHEn7R8oYiHyIAeEB-jBG4lYAwzbtnfpzt-OMFWIIyEhgQcGoQS3cpFn3Fy7M4uDqB4Ca9YdWlxFP31q5D2dJRnMReg_fFz9iVAq6MqUiujlnkRqQzVHgNQiaJ2CZv-YgAxwVBUXQXUCOUf7L_iW49bXe5bX3vpVDuqIAjB_NcPXPnBm3Q0fC0Q", DateTime.Now.AddDays(1));
-        }
-
-        public override ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
-        {
-            return new ValueTask<AccessToken>(new AccessToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Imh1Tjk1SXZQZmVocTM0R3pCRFoxR1hHaXJuTSIsImtpZCI6Imh1Tjk1SXZQZmVocTM0R3pCRFoxR1hHaXJuTSJ9.eyJhdWQiOiIxZDYzMDljOS0zZTIyLTQyZDgtYmI1OS0zMzQxYTZmODYzMmIiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8wMjBiMGNmMy1kNmIyLTQ2NGUtOWIyZC00NWUxMjQyNDQ0MjgvIiwiaWF0IjoxNTk1ODc0ODYyLCJuYmYiOjE1OTU4NzQ4NjIsImV4cCI6MTU5NTg3ODc2MiwiYWlvIjoiRTJCZ1lMRFF1LzA1N3VQNVlsdnp5N3RtbnhRUUFBQT0iLCJhcHBpZCI6ImM2NGZlYjhlLTQ1NDUtNGYyYy1hMGRkLTZiNzJhOGQxYThiYiIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzAyMGIwY2YzLWQ2YjItNDY0ZS05YjJkLTQ1ZTEyNDI0NDQyOC8iLCJvaWQiOiJhOGE4YThlYS03YzczLTQ1MmItYTQ4ZC03ZjgyMDFlNGM0MmUiLCJzdWIiOiJhOGE4YThlYS03YzczLTQ1MmItYTQ4ZC03ZjgyMDFlNGM0MmUiLCJ0aWQiOiIwMjBiMGNmMy1kNmIyLTQ2NGUtOWIyZC00NWUxMjQyNDQ0MjgiLCJ1dGkiOiIzR2dVeTQtMWRreTFyZ3pMMDNKWkFBIiwidmVyIjoiMS4wIn0.1_BLIyW-6CdmhenzNknz6ouZGBNGx9G75ruhRcX_JwOFlyq2W3GffqKPWZ9Yb23OxeE4eSwpyh2UWwEAXbgwGq_NVASNrLw6Tn4KLl2erX6fTM11F56Vp_lqR3Fz3NJA3Hesnso19x_cinDrC_oNV8BTrtM5ltAFHEn7R8oYiHyIAeEB-jBG4lYAwzbtnfpzt-OMFWIIyEhgQcGoQS3cpFn3Fy7M4uDqB4Ca9YdWlxFP31q5D2dJRnMReg_fFz9iVAq6MqUiujlnkRqQzVHgNQiaJ2CZv-YgAxwVBUXQXUCOUf7L_iW49bXe5bX3vpVDuqIAjB_NcPXPnBm3Q0fC0Q", DateTime.Now.AddDays(1)));
-        }
-    }
-
 
     class Program
     {
@@ -39,7 +22,7 @@ namespace ConsoleAppStorageQueue
             client3.SendMessage("StorageSharedKeyCredential" + DateTime.UtcNow);
 
             // Create a SAS token that's valid for one hour.
-            var sasBuilder = new QueueSasBuilder()
+            var sasBuilder = new QueueSasBuilder
             {
                 QueueName = "example-q",
                 StartsOn = DateTimeOffset.UtcNow,
@@ -53,27 +36,16 @@ namespace ConsoleAppStorageQueue
             string sasToken = sasBuilder.ToSasQueryParameters(s).ToString();
 
             // Construct the full URI, including the SAS token.
-            UriBuilder fullUri = new UriBuilder()
+            UriBuilder fullUri = new UriBuilder
             {
                 Scheme = "https",
-                Host = string.Format("{0}.queue.core.windows.net", "stefsa"),
-                Path = string.Format("{0}", "example-q"),
+                Host = "stefsa.queue.core.windows.net",
+                Path = "example-q",
                 Query = sasToken
             };
 
             var q4 = new QueueClient(fullUri.Uri, null);
             q4.SendMessage("QueueSasBuilder " + DateTime.UtcNow);
-
-            //QueueServiceClient qServiceClient = new QueueServiceClient(new Uri("https://stefsa.queue.core.windows.net/example-q"), new DefaultAzureCredential());
-            //// Get a user delegation key for the Blob service that's valid for seven days.
-            //// You can use the key to generate any number of shared access signatures over the lifetime of the key.
-            //UserDelegationKey key = qServiceClient.GetUserDelegationKey(DateTimeOffset.UtcNow, DateTimeOffset.UtcNow.AddDays(7));
-
-
-            //TokenCredential tc = new T();
-            // var x = new StorageSharedKeyCredential();
-
-            //StorageCredentials storageCredentials = new StorageCredentials(tokenCredential);
 
             Console.WriteLine("tc");
             var tc = new ClientSecretCredential("020b0cf3-d6b2-464e-9b2d-45e124244428", "4bd5d21b-1a20-4e86-a06a-b7f8598030b3", "jf864wuqkGDGQHf5uYt--be~5nlc3r-nX8");
