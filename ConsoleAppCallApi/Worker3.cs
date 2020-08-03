bb38f52843d331ebe214f6da4db9da4bd148dbed
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-08-03 08:32:22 +0000

    .

--- ConsoleAppCallApi/Worker3.cs
+++ ConsoleAppCallApi/Worker3.cs
@@ -1,17 +1,21 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using System;
+using Microsoft.Extensions.Hosting;
 using Stef.AuditClient.MicrosoftIdentityClient;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ConsoleAppCallApi
 {
     public class Worker3AzureIdentity : BackgroundService
     {
         private readonly IAuditClientMicrosoftIdentityClient _client;
+        private readonly IServiceProvider _serviceProvider;
 
-        public Worker3AzureIdentity(IAuditClientMicrosoftIdentityClient client)
+        public Worker3AzureIdentity(IAuditClientMicrosoftIdentityClient client, IServiceProvider serviceProvider)
         {
             _client = client;
+            _serviceProvider = serviceProvider;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -19,6 +23,9 @@ namespace ConsoleAppCallApi
             while (!stoppingToken.IsCancellationRequested)
             {
                 await _client.GetAsync(stoppingToken);
+
+                var c2 = _serviceProvider.GetRequiredService<IAuditClientMicrosoftIdentityClient>();
+                await c2.GetAsync(stoppingToken);
 
                 await Task.Delay(10000, stoppingToken);
             }
